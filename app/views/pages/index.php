4a545468f16bd38ba91ca041ae505a2e6c4a602c
Robert Gumeny <robert.gumeny528@gmail.com>
2020-08-25 09:28:37 -0600

    init

---
+++ app/views/pages/index.php
@@ -0,0 +1,4 @@
+<?php require APPROOT . '/views/inc/header.php';?>
+<h1><?php echo $data['title'];?></h1>
+<p>This is the GumenyMVC PHP framework. Please refer to the documentation for information about how to use it.</p>
+<?php require APPROOT . '/views/inc/footer.php';?>
